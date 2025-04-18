63032755df9335d82c3a6cf92e17b8c75887f738
dmtan14 <158117413+dmtan14@users.noreply.github.com>
2025-04-18 17:23:58 +0800

    enh: footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ export default function Footer() {
     <footer className="w-full bg-white text-pixelBlue-700 italic py-1 border-t border-gray-200">
       <div className="flex justify-center">
         <p className="text-sm sm:text-base font-semibold translate-y-1.5">
-          Powered by IS215 2024–2025 · Pixel8 Co.
+          Powered by Pixel8 Co. · IS215 2024–2025 
         </p>
       </div>
     </footer>
