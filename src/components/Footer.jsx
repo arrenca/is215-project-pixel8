3ec632492d8ca6ce0a0cffe98102a9d0f33c5ef1
Deson Maquilang <58901455+Deson-Maquilang@users.noreply.github.com>
2025-05-03 09:14:18 +0800

    Update Footer Responsiveness

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,11 @@
 export default function Footer() {
   return (
-    <footer className="w-full bg-white text-pixelBlue-700 italic py-1 border-t border-gray-200">
-      <div className="flex justify-center">
-        <p className="text-sm sm:text-base font-semibold translate-y-1.5">
+    <footer className="w-full bg-white text-pixelBlue-700 italic py-2 border-t border-gray-200">
+      <div className="flex justify-center px-4">
+        <p className="text-xs sm:text-sm md:text-base font-semibold text-center">
           Powered by Pixel8 Co. · IS215 2024–2025 
         </p>
       </div>
     </footer>
-  )
+  );
 }
